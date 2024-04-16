941ed92278d6a3bf2f30c40a837c560357e3ffd0
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-04-16 17:18:16 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.js
@@ -0,0 +1,8 @@
+function solution(my_string) {
+    return my_string
+        .split(/[^0-9]/g)
+        .map(Number)
+        .reduce((acc, cur) => {
+            return (acc += cur);
+        });
+}
