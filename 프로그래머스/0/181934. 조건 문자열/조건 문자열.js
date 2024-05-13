7e9ff2de91987e422d10a3cec094ca52bbdc8ce3
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-05-13 14:47:23 +0900

    [level 0] Title: 조건 문자열, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,20 @@
+function solution(ineq, eq, n, m) {
+ 
+   if (ineq === ">" && eq === "=") {
+    return n >= m ? 1 : 0;
+  }
+
+  if (ineq === "<" && eq === "=") {
+    return n <= m ? 1 : 0;
+  }
+
+  if (ineq === ">" && eq === "!") {
+    return n > m ? 1 : 0;
+  }
+
+  if (ineq === "<" && eq === "!") {
+    return n < m ? 1 : 0;
+  }
+ return -1;
+    
+}
