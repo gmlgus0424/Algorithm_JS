2bf09ef32eb09a60527c8306ca1cbc77c67835eb
choi hee jin <81346229+gmlgus0424@users.noreply.github.com>
2024-08-27 16:46:42 +0900

    Update 커피 심부름.js

--- 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.js
@@ -1,1 +1,1 @@
-const solution = (order) => order.reduce((acc, cur) => acc + (cur.includes('tte') ? 5000 : 4500), 0)
+const solution = (order) => order.reduce((acc, cur) => acc + (cur.includes('coffee') ? 5000 : 4500), 0)
